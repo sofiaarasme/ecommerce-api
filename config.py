627b6e364e7a41489ef8d865dd171ec2a6a22959
sofiaarasme <111990944+sofiaarasme@users.noreply.github.com>
2024-12-13 14:46:31 -0400

    build: :package: database configuration updated

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ import os
 
 load_dotenv()
 
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:123456@localhost:5432/ecommerce_db")
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:123456@localhost:5433/ecommerce_db")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
+from sqlalchemy import text
 from config import Base, engine, get_db
 
+# Esto asegura que las tablas de la base de datos se creen
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
@@ -11,10 +13,10 @@ def read_root():
     return {"message": "Hello, World!"}
 
 @app.get("/test-db")
-def test_database(db: Session = Depends(get_db)):
+async def test_db():
     try:
-        # Prueba simple para asegurar que la conexión funcione
-        db.execute("SELECT 1")
-        return {"message": "Database connection successful"}
+        with engine.connect() as connection:
+            connection.execute(text("SELECT 1"))
+        return {"message": "Connection successful"}
     except Exception as e:
         return {"error": str(e)}
