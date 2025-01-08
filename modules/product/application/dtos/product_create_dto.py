931b018f6ac0eba355309001ffc922ffa273a88e
sofiaarasme <111990944+sofiaarasme@users.noreply.github.com>
2025-01-08 15:56:54 -0400

    fix: :bug: product price calculation

--- modules/product/application/dtos/product_create_dto.py
+++ modules/product/application/dtos/product_create_dto.py
@@ -1,10 +1,18 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, validator
 
 class ProductCreateDto(BaseModel):
     code: str
     name: str
     description: str
-    price: float
-    margin: float
+    margin: float = Field(..., gt=0, lt=1, description="Margin should be a value between 0 and 1")
     cost: float
     is_active: bool = True
+    price: float = None
+    
+    @validator('price', pre=True, always=True)
+    def calculate_price(cls, v, values):
+        cost = values.get('cost')
+        margin = values.get('margin')
+        if cost is not None and margin is not None:
+            return cost / (1 - margin)
+        return v
